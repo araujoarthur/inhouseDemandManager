41026775f816533cc3414f12a29e7bf6af219c25
Arthur <arthur.galdino@edu.pucrs.br>
2022-12-19 00:43:33 -0300

    upgrades on idmSQLmanager module. Corrections on legacy version. legacy file isn't supported anymore

--- idmSQLmanager.py
+++ idmSQLmanager.py
@@ -6,6 +6,9 @@ import sys
 class idmSQLmanager:
     config = {}
     def __init__(self, user='', password='', host='127.0.0.1', port='3306', database=''):
+        """ Creates an idmSQLmanager object. 
+        Autocommit and Cursor Dictionary are Enabled by default.
+        """
         try:
             self.connection = mariadb.connect(
                                             user=user,
@@ -14,31 +17,124 @@ class idmSQLmanager:
                                             port=port,
                                             database=database
                                             )
+            self.config = {
+                'user' = user,
+                'password' = password,
+                'host' = host,
+                'port' = port,
+                'database' = database
+            }
+            self.connection.autocommit = True
         except Exception as e:
             print(f"Failed to Connect into MariaDB. \nERROR: {e}")
             sys.exit(1)
         
-        self.cursor = self.connection.cursor()
+        self.cursor = self.connection.cursor(dictionary=True)
 
         return None
+
+
+    def disableCursorDicitonary(self):
+        """
+            Disables dictionary into cursor result for cases where it'd be desidered to handle tuples.
+        """
+        self.cursor = self.connection.cursor(dictionary=False)
+        return True
+
+    def enableCursorDictonary(self):
+        """
+            Enables dictionary into cursor result so it's easier to deal with resulting data from queries.
+        """
+        self.cursor = self.connection.cursor(dictionary=True)
+        return True
+
+
+    def enableAutoCommit(self):
+        """
+            Enables autocommit to use idmSQLmanager.execute() method.
+        """
+        self.connection.autocommit = True
+        return True
     
-def execute(query, *args):
-    import sqlparse
-    print(sqlparse.parse(sqlparse.format(query, strip_comments=True).strip()))
 
-execute("SELECT * FROM FOO")
+    def disabelAutoCommit(self):
+        """
+            Enables autocommit to use idmSQLmanager.Transactions methods.
+        """
+        self.connection.autocommit = False
+        return True
+
+
+    def checkQuery(self, query):
+        """
+            Check on query for multiple statements, validity of operaton and existence of statement.
+        """
+        # https://sqlparse.readthedocs.io/en/latest/api/
+        import sqlparse
+
+        # parse into a list of statements and each statement into a list of tokens
+        statList = sqlparse.parse(sqlparse.format(query, strip_comments=True).strip())
+
+        # Not sure about MariaDB dealing multiple statements.
+        if len(statList) > 1:
+            raise RuntimeError('Can\t handle more than one statement')
+        elif len(statList) == 0:
+            raise RuntimeError('Missing Statement')
+        elif not(statList[0][0].value in ["UPDATE", "INSERT", "SELECT", "DELETE"]):
+            raise RuntimeError('Not a valid operation')
+
+        return None
+
+
+    # Based on https://github.com/cs50/python-cs50/blob/main/src/cs50/sql.py
+    def execute(self, query, *args):
+        if self.connection.autocommit == False:
+            raise RuntimeError('Can\'t use execute while transactions are enabled.')
+        
+        self.checkQuery(query)
+
+        qmarkCount = query.count('?')
+        if qmarkCount > len(args):
+            raise RuntimeError('More placeholders than arguments')
+        elif qmarkCount < len(args):
+            raise RuntimeError('More arguments than placeholders')
+        
+        currReadyArgs = tuple(args)
+
+        self.cursor.execute(query, currReadyArgs)
+        self.connection.cursor(dictionary=True)
+
+        print(self.cursor.fetchall())
     
+    class Transaction:
+        def __init__():
+            if self.connection.autocommit == True:
+                raise RuntimeError('Can\'t use transactions while autocommit is active')
+                
+        def addStatement(query, *args):
+            if self.connection.autocommit == True:
+                raise RuntimeError('Can\'t use transactions while autocommit is active')
 
+            self.checkQuery(query)
 
+            qmarkCount = query.count('?')
+            if qmarkCount > len(args):
+                raise RuntimeError('More placeholders than arguments')
+            elif qmarkCount < len(args):
+                raise RuntimeError('More arguments than placeholders')
+            
 
 
 config = {
     'user':'root',
     'password':'root',
     'host':'127.0.0.1',
-    'port':'3306',
+    'port':3306,
     'database':'familymanager'
 }
 
 d = idmSQLmanager(**config)
+d.enableAutoCommit()
+print(d.execute("SELECT * FROM test"))
+print(d.execute("SELECT * FROM test WHERE val1=?", 1))
 
